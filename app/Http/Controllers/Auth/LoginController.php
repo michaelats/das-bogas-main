38ff63f6171a1f16d19065331863a7aabbf4d0ab
michaelats <strs.michaels@gmail.com>
2023-05-01 14:01:16 +0700

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt(array('email' => $input['email'], 'password' =>
+        $input['password']))) {
+            if (auth()->user()->role == 1) {
+                return redirect('/dashboard')->with('success', "Berhasil masuk!");
+            } elseif (auth()->user()->role == 0) {
+                return redirect('/')->with('success', "Berhasil masuk!");
+            }
+        } else {
+            Alert::warning('Failed', 'Email atau sandi Salah!');
+            return redirect()->route('login');
+        }
+    }
+}
